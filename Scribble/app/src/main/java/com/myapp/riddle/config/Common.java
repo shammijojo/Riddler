3e3e295d6eecf9dc5496034680df682dad930a64
Shammi <shammijojo22@gmail.com>
2021-11-15 18:53:55 +0530

    Classes added

---
+++ Scribble/app/src/main/java/com/myapp/riddle/config/Common.java
@@ -0,0 +1,73 @@
+package com.myapp.riddle.config;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.view.MenuItem;
+
+import com.myapp.riddle.Homepage;
+import com.myapp.riddle.Leaderboard;
+import com.myapp.riddle.database.Database;
+import com.myapp.riddle.database.Firebase;
+
+public class Common {
+
+    public boolean validateCompletion(Activity activity){
+        if(Integer.parseInt(new Database(activity).getFromDb("level"))>Constants.TOTAL_QUE){
+            activity.finish();
+            Intent i = new Intent(activity, Leaderboard.class);
+            activity.startActivity(i);
+            return true;
+        }
+        return false;
+    }
+
+    public boolean createAlertDialog(final Activity activity, MenuItem item, final Database db){
+        final Firebase firebase=new Firebase();
+        if(item.getTitle().toString().equals(Constants.LEADERBOARD)){
+            Intent i=new Intent(activity,Leaderboard.class);
+            if(!activity.getClass().equals(Leaderboard.class))
+            activity.startActivity(i);
+        }
+        else if(item.getTitle().toString().equals(Constants.EXIT)){
+            activity.moveTaskToBack(true);
+            System.exit(0);
+        }
+        else if(item.getTitle().toString().equals(Constants.RESTART_GAME)){
+            AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(activity);
+            alertDialogBuilder.setMessage("Are you sure to restart?");
+            alertDialogBuilder.setCancelable(true);
+
+            alertDialogBuilder.setPositiveButton(
+                    "Yes",
+                    new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id) {
+                            dialog.cancel();
+                            db.updateUserInfo(Constants.LEVEL,1);
+                            db.updateUserInfo(Constants.SCORE,0);
+                            firebase.updateScore(db.getFromDb("name"),0);
+                            Intent i=new Intent(activity,Homepage.class);
+                            activity.startActivity(i);
+                        }
+                    });
+
+            alertDialogBuilder.setNegativeButton(
+                    "No",
+                    new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id) {
+                            dialog.cancel();
+                        }
+                    });
+
+            AlertDialog alertDialog = alertDialogBuilder.create();
+            alertDialog.show();
+
+        }
+
+        return true;
+
+    }
+
+
+}
---
+++ Scribble/app/src/main/java/com/myapp/riddle/config/Constants.java
@@ -0,0 +1,33 @@
+package com.myapp.riddle.config;
+
+import com.myapp.riddle.Homepage;
+import com.myapp.riddle.Leaderboard;
+import com.myapp.riddle.LeaderboardList;
+import com.myapp.riddle.MainActivity;
+import com.myapp.riddle.Riddle;
+
+public class Constants {
+
+    public static  final Integer TOTAL_QUE=59;
+
+    public static final String LEVEL="level";
+    public static final String NAME="name";
+    public static final String SCORE="score";
+    public static final String PIC="pic";
+
+    public static final String LEADERBOARD="Leaderboard";
+    public static final String RESTART_GAME="Restart Game";
+    public static final String EXIT="Exit";
+
+
+    public static final String START_THE_GAME="START THE GAME";
+    public static final String CONTINUE_THE_GAME="CONTINUE THE GAME";
+
+    public static final Class HOMEPAGE_CLASS= Homepage.class;
+    public static final Class LEADERBOARD_CLASS= Leaderboard.class;
+    public static final Class LEADERBOARD_LIST_CLASS= LeaderboardList.class;
+    public static final Class MAIN_ACTIVITY_CLASS= MainActivity.class;
+    public static final Class RIDDLE_CLASS= Riddle.class;
+
+
+}
